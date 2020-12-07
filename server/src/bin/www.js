b4c524e589a705c932bcb9fde70cb1325733de36
bseiryk <pheerbah@gmail.com>
2020-12-07 18:27:40 +0200

    clean www file

--- server/src/bin/www.js
+++ server/src/bin/www.js
@@ -4,7 +4,6 @@ import path from 'path';
 import bodyParser from 'body-parser';
 import { Server } from 'http';
 import iio from 'socket.io';
-const { ExpressPeerServer } = require('peer');
 
 
 
@@ -12,13 +11,8 @@ const { ExpressPeerServer } = require('peer');
 const app = express();
 const server = Server(app)
 const io = iio(server)
-// const peerServer = ExpressPeerServer(server, {
-//   path: '/myapp'
-// });
 
 app.use(cors({
-  origin: ["http://localhost:3000", "http://localhost:2222", "http://192.168.0.27:8888"],
-  methods: ['GET', 'PUT', 'POST', 'DELETE', 'OPTION'],
   allowedHeaders: ['Content-Type'],
   credentials: true,
   optionsSuccessStatus: 200,
@@ -29,19 +23,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-// app.use('/peerjs', peerServer);
-
 
 io.on('connection', socket => {
-  // socket.on('send-offer-to-server', offer => {
-  //   socket.broadcast.emit('send-offer-to-clients', offer);
-  // })
-  // socket.on('send-answer-to-server', answer => {
-  //   socket.broadcast.emit('send-answer-to-clients', answer);
-  // })
-  // socket.on('send-ice-candidate-to-server', candidate => {
-  //   socket.broadcast.emit('send-ice-candidate-to-clients', candidate);
-  // })
   socket.on('join-room', (roomId, userId) => {
 
     socket.join(roomId);
